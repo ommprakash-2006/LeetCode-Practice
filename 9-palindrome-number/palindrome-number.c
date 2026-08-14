bc024ac4846834f99f4b8702285367f4b4fff28a
Omm Prakash Sahoo <ommprakashsahoo6@gmail.com>
2026-08-14 18:06:55 +0530

    Time: 1 ms (51.77%) | Memory: 8.9 MB (23.04%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.c
@@ -0,0 +1,12 @@
+bool isPalindrome(int x) {
+    long long rev = 0;
+    int temp = x;
+
+    while (temp > 0) {
+        int rem = temp % 10;
+        rev = rev * 10 + rem;
+        temp = temp / 10;
+    }
+
+    return rev == x;
+}
