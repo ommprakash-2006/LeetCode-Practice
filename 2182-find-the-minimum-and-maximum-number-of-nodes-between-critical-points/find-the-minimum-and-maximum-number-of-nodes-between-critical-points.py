86edb2f6e9c9cfdf38623903eaf470ab298cdfdc
Omm Prakash Sahoo <ommprakashsahoo6@gmail.com>
2026-09-01 17:12:04 +0530

    Time: 215 ms (72.73%) | Memory: 93 MB (41.82%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.py
@@ -1,31 +1,47 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
-#         self.next = next
-class Solution:
-    def nodesBetweenCriticalPoints(self, head: Optional[ListNode]) -> List[int]:
-        prev=head
-        cur=head.next
-        first_cp=-1
-        second_cp=-1
-        min_dist=1e9
-        index=1
-        while cur and cur.next:
-            nxt=cur.next
-            maxima=cur.val>prev.val and cur.val>nxt.val
-            minima=cur.val<prev.val and cur.val<nxt.val
-            if minima or maxima:
-                if first_cp==-1:
-                    first_cp=index
-                else:
-                    min_dist=min(min_dist,index-second_cp)
-                second_cp=index
-            prev=cur
-            cur=nxt
-            index=index+1
-        if min_dist==1e9:
-            return [-1,-1]
-        max_dist=second_cp-first_cp
-        return [min_dist,max_dist]
-        
+#         self.next = nextS
+class Solution(object):
+    def nodesBetweenCriticalPoints(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: List[int]
+        """
+
+        positions = []
+
+        prev = head
+        curr = head.next
+        pos = 1
+
+        while curr.next:
+            next_node = curr.next
+
+            # Check if current node is a critical point
+            if (curr.val > prev.val and curr.val > next_node.val) or \
+               (curr.val < prev.val and curr.val < next_node.val):
+                positions.append(pos)
+
+            prev = curr
+            curr = next_node
+            pos += 1
+
+        # Less than 2 critical points
+        if len(positions) < 2:
+            return [-1, -1]
+
+        # Minimum distance
+        min_distance = float('inf')
+
+        for i in range(1, len(positions)):
+            distance = positions[i] - positions[i - 1]
+
+            if distance < min_distance:
+                min_distance = distance
+
+        # Maximum distance
+        max_distance = positions[-1] - positions[0]
+
+        return [min_distance, max_distance]
