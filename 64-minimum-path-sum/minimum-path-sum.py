fc85229a75e59048bf3c9476660925ab51164560
Omm Prakash Sahoo <ommprakashsahoo6@gmail.com>
2026-09-25 16:27:59 +0530

    Time: 27 ms (22.62%) | Memory: 32.8 MB (5.22%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,18 +1,13 @@
 from functools import cache
 class Solution:
     def minPathSum(self, grid: list[list[int]]) -> int:
-        
         m = len(grid)
         n = len(grid[0])
-
         @cache
-        def solve(i, j):
+        def func(i, j):
             if i == 0 and j == 0:
                 return grid[0][0]
-
             if i < 0 or j < 0:
-                return float('inf')
-
-            return grid[i][j] + min(solve(i - 1, j), solve(i, j - 1))
-
-        return solve(m - 1, n - 1)
+                return 1e9
+            return grid[i][j] + min(func(i - 1, j), func(i, j - 1))
+        return func(m - 1, n - 1)
